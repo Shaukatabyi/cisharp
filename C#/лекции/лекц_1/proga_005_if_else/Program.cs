8eb1d62b4f68e531a9c3315d670c4b6136c5936d
Шаукат <shaukat.off@mail.ru>
2022-11-22 00:33:31 +0300

    С#

---
+++ C#/лекции/лекц_1/proga_005_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Как Вас зовут? ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Привеет, это же Маша!!!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}    
