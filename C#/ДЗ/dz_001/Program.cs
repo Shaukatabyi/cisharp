b314f1824f4797c00bc6d1b2d0f475313f209625
Шаукат <shaukat.off@mail.ru>
2022-11-22 22:59:57 +0300

    нахождение мах из двух чисел

--- C#/ДЗ/dz_001/Program.cs
+++ C#/ДЗ/dz_001/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a =7;
+int b = -3;
+int max = 0;
+
+if (a>b)
+{
+    max=a;
+}
+else
+{
+    max = b;
+}
+Console.Write("a = ");
+Console.Write(a);
+Console.Write("; ");
+Console.Write("b = ");
+Console.Write(b);
+Console.Write(" -> ");
+Console.Write("max = ");
+Console.WriteLine(max);
