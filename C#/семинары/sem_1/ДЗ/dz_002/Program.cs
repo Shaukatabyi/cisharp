1466f9a86b857efef0034996f64dfc2cedd50ba7
Шаукат <shaukat.off@mail.ru>
2022-11-23 00:26:21 +0300

    нахождение мах из 3 чисел

---
+++ C#/семинары/sem_1/ДЗ/dz_002/Program.cs
@@ -0,0 +1,21 @@
+﻿int a = 2;
+int b = 9;
+int c = 18;
+
+int max = a;
+
+if(b>max)
+{
+    max = b;
+}
+if(c>max)
+{
+    max = c;
+}
+Console.Write(a);
+Console.Write(", ");
+Console.Write(b);
+Console.Write(", ");
+Console.Write(c);
+Console.Write(" -> ");
+Console.Write(max);
