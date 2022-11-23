f5919cdbcebdb0c18837584bf6d3f510e480100b
Шаукат <shaukat.off@mail.ru>
2022-11-23 17:53:19 +0300

    задача 4

---
+++ C#/семинары/sem_1/ДЗ/dz_004/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+string s = Console.ReadLine();
+int n = Convert.ToInt32(s);
+int count = 1;
+while(count <= n)
+{
+if (count % 2 == 0)
+{
+Console.WriteLine(count);
+
+}
+count = count + 1;
+}
