781ffab125c24c6b9c1e49b3d7cb582590027d00
Шаукат <shaukat.off@mail.ru>
2022-11-25 00:38:41 +0300

    дз зад.3 день недели

---
+++ C#/семинары/sem_2/dz/zad_3/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+Console.WriteLine("Введите цифру дня недели ");
+
+string s = Console.ReadLine();
+int num = Convert.ToInt32(s);
+
+if (num < 1) 
+{
+    Console.WriteLine("такого дня недели нет");
+}
+if (num > 7) 
+{
+    Console.WriteLine("такого дня недели не существует");
+}
+
+if (num == 6) 
+{
+    Console.WriteLine("да");
+}
+
+if (num == 7) 
+{
+    Console.WriteLine("да");
+}
+
+else
+{
+    Console.Write("нет");
+}
