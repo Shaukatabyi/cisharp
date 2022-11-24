86954b63e2bdfde41ec07503e8c2cc9b07bd0fef
Шаукат <shaukat.off@mail.ru>
2022-11-24 23:31:26 +0300

    дз задача 1 вывод 2 цифры надо доделать

--- C#/семинары/sem_2/dz/zad_1/Program.cs
+++ C#/семинары/sem_2/dz/zad_1/Program.cs
@@ -1,6 +1,22 @@
-﻿int num = 782;
+﻿Console.Clear();
+Console.WriteLine("Введите трёхзначное число ");
 
-int des = num/10%10;
-Console.Write(num);
-Console.Write(" -> ");
-Console.Write(des);
+string s = Console.ReadLine();
+int num = Convert.ToInt32(s);
+
+if (num > 999)
+{
+    Console.WriteLine("error");
+}
+if (num < 100)
+{
+    Console.WriteLine("error");
+}
+
+
+else
+{
+    int des = num / 10 % 10;
+    Console.Write("вторая цифра ->  ");
+    Console.Write(des);
+}
