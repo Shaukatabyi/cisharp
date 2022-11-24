3ff69a54d77e1fb89cf3cdbf41134156d7ff32eb
Шаукат <shaukat.off@mail.ru>
2022-11-24 23:48:53 +0300

    дз зад.2 вывод 3 цифры

--- C#/семинары/sem_2/dz/zad_2/Program.cs
+++ C#/семинары/sem_2/dz/zad_2/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿Console.Clear();
+Console.WriteLine("Введите любое число ");
+
+string s = Console.ReadLine();
+int num = Convert.ToInt32(s);
+
+if (num < 100)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+
+else
+{
+    int des = num /100 % 10;
+    Console.Write("третья цифра ->  ");
+    Console.Write(des);
+}
