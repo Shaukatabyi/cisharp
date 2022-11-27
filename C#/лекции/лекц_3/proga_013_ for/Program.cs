58cd86ebcbb62b5478f47f737b783029c2aab623
Шаукат <shaukat.off@mail.ru>
2022-11-27 11:19:41 +0300

    proga14-- табл умножения

---
+++ C#/лекции/лекц_3/proga_013_ for/Program.cs
@@ -0,0 +1,13 @@
+﻿string Method4(int count, string text) // что то принимают и что то возвращают
+{
+    
+    string result = String.Empty;
+    for(int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return  result;
+}
+string res = Method4(5, "Сэлэм ");
+Console.WriteLine(res);
+
