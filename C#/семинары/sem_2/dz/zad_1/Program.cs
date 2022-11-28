2b0165d124cd54738ee65f457debab20eb87b08f
Шаукат <shaukat.off@mail.ru>
2022-11-28 10:37:38 +0300

    комит после дз

--- C#/семинары/sem_2/dz/zad_1/Program.cs
+++ C#/семинары/sem_2/dz/zad_1/Program.cs
@@ -1,23 +1,26 @@
-﻿Console.Clear();
-Console.WriteLine("Введите трёхзначное число ");
-
-string s = Console.ReadLine();
-int num = Convert.ToInt32(s);
-
-if (num > 999)
+﻿// 4. метод, формирующий новый массив на основе имеющегося
+//! ДЗ ОПИСАТЬ БЛОК-СХЕМУ РЕШЕНИЯ ЗАДАЧИ
+int[] Solution(int[] collectionElements, int evenCounts)
 {
-    Console.WriteLine("error");
-}
 
-else
-{
-    if (num < 100)
+  //       v
+  // 1 2 3 4 5 6 7 8 9 0 collectionElements
+  // 2 0 0 0 0 items
+  //   ^ 
+
+  int[] items = new int[evenCounts];
+  int length = collectionElements.Length;
+  int position = 0;
+  int indexInItems = 0;
+  while (position < length)
+  {
+    if (collectionElements[position] % 2 == 0)
     {
-    Console.WriteLine("error");
+      items[indexInItems] = collectionElements[position];
+      indexInItems++;
     }
-    else 
-    {int des = num / 10 % 10;
-    Console.Write("вторая цифра ->  ");
-    Console.Write(des);
-    }
+    position++;
+  }
+
+  return items;
 }
