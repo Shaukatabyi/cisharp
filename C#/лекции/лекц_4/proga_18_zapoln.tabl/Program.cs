10cea541e2e6b1bc7873f6c5b8dfeca3ef8d7dfa
Шаукат <shaukat.off@mail.ru>
2022-11-28 16:28:38 +0300

    начал делать прогу 18

---
+++ C#/лекции/лекц_4/proga_18_zapoln.tabl/Program.cs
@@ -0,0 +1,13 @@
+﻿void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+     for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        Console.Write($"{matr[i, j]} ");
+    }
+    Console.WriteLine();   
+    }
+}   
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
