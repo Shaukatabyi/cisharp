90a439bd9329d0440c9ca0aeb08c6736fadb24b2
Шаукат <shaukat.off@mail.ru>
2022-11-29 21:19:36 +0300

    дз_2 сем _4

---
+++ C#/семинары/sem_4/dz/zadacha_2/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача 27: Напишите метод, который принимает на вход число
+ и выдаёт сумму цифр в числе.
+
+ 9012 -> 12*/
+
+int sum (int array)
+{   int newArray = array;
+    int summa = 0;
+       while (newArray >= 1)
+    {
+        summa = summa + newArray%10;
+        newArray = newArray/10;
+    } 
+     return  summa;
+}
+int result = sum(9012);
+Console.WriteLine(result);
