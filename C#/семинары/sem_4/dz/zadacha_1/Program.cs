12277d2753427b019066a90e3671208bf11b6c56
Шаукат <shaukat.off@mail.ru>
2022-11-29 15:32:26 +0300

    дз_1 сем _4

---
+++ C#/семинары/sem_4/dz/zadacha_1/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 25: Напишите метод, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+Math.Pow использовать нельзя
+
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+double f(double A, double B)
+{
+    double result = 1;
+    for (double i = 0; i < B; i++)
+    {
+         result = result*A;
+    }
+    return result;
+}
+double re = f(2, 4);
+Console.WriteLine(re);
