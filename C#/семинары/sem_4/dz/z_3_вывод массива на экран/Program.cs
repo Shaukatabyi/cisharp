9aa2f54de535173a2fe09e960ca8f46217e8c3b7
Шаукат <shaukat.off@mail.ru>
2022-11-30 17:04:09 +0300

    дз_3  сем _4

---
+++ C#/семинары/sem_4/dz/z_3_вывод массива на экран/Program.cs
@@ -0,0 +1,55 @@
+﻿/*Задача 29:
+ Напишите методы, который задаёт массив из 8 элементов
+ и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]*/
+// 1. метод, создающий массив с указанным количеством эл.
+
+int[] func(int count)
+{
+    return new int[count];
+}
+int countElements = 8;
+int[] arr = func(countElements);
+
+Console.WriteLine(arr);
+
+
+// 2. метод, заполняющий массив поэлементно *от пользователя
+
+void Fill(int[] array)
+{
+  int size = array.Length;
+
+  int index = 0;
+  while (index < size)
+  {
+    array[index] = GetNumber($"array[{index}]: ");
+    index++;
+  }
+}
+
+
+ //5.2 метод, печатающий массив 
+string PrintGood(int[] numbers)
+{
+    int size = numbers.Length;
+    int i = 0;
+    string result = "[ ";
+
+    while (i < size)
+    {
+        result += ($"{numbers[i]} ");
+        i++;
+    }
+    return result + "]";
+}
+
+
+
+Fill(arr);
+PrintGood(arr);
+
+Console.WriteLine(PrintGood(arr));
