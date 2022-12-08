85d0b660156f48aa2aafb075d020bae84343e426
Шаукат <shaukat.off@mail.ru>
2022-12-08 11:29:02 +0300

    дз 3 сем 8 всеми правдами и неправдами

--- C#/zagotovki/Program.cs
+++ C#/zagotovki/Program.cs
@@ -248,6 +248,45 @@ void MinStroka(int[,]matr)
     Console.WriteLine($"строка {strminsum+1} - с минимальной суммой элементов");
 }
 
+// Функция вывода индекса элементов 3D массива
+void Print3D(int[, ,] mass3d)
+{
+    for (int i = 0; i < mass3d.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass3d.GetLength(1); j++)
+        {
+            System.Console.WriteLine();
+            for (int k = 0; k < mass3d.GetLength(2); k++)
+            {
+                System.Console.Write($"{mass3d[i,j,k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
+// Функция заполнения 3D массива не повторяющимеся числами
+ void Zapol3D(int[, ,] massiv3d)
+ {
+    int tsifra = 10;
+    for (int i = 0; i < massiv3d.GetLength(0); i++)
+    {
+        for (int j = 0; j < massiv3d.GetLength(1); j++)
+        {
+            for (int k = 0; k < massiv3d.GetLength(2); k++)
+            {
+                massiv3d[i,j,k] = tsifra;
+                tsifra += 1;
+            }
+        }
+    }
+ }
+
+int[,,] array3D = new int[2, 2, 2];
+Zapol3D(array3D);
+System.Console.WriteLine(array3D);
+Print3D(array3D);
+System.Console.WriteLine(array3D);
+
+
 
 
 
--- C#/семинары/sem_8/dz/dz3/Program.cs
+++ C#/семинары/sem_8/dz/dz3/Program.cs
@@ -6,3 +6,41 @@
 34(1,0,0) 41(1,1,0)
 27(0,0,1) 90(0,1,1)
 26(1,0,1) 55(1,1,1)*/ 
+
+// Функция вывода индекса элементов 3D массива
+void Print3D(int[, ,] mass3d)
+{
+    for (int i = 0; i < mass3d.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass3d.GetLength(1); j++)
+        {
+            System.Console.WriteLine();
+            for (int k = 0; k < mass3d.GetLength(2); k++)
+            {
+                System.Console.Write($"{mass3d[i,j,k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
+// Функция заполнения 3D массива не повторяющимеся числами
+ void Zapol3D(int[, ,] massiv3d)
+ {
+    int tsifra = 10;
+    for (int i = 0; i < massiv3d.GetLength(0); i++)
+    {
+        for (int j = 0; j < massiv3d.GetLength(1); j++)
+        {
+            for (int k = 0; k < massiv3d.GetLength(2); k++)
+            {
+                massiv3d[i,j,k] = tsifra;
+                tsifra += 1;
+            }
+        }
+    }
+ }
+
+int[,,] array3D = new int[2, 2, 2];
+Zapol3D(array3D);
+System.Console.WriteLine(array3D);
+Print3D(array3D);
+System.Console.WriteLine(array3D);
